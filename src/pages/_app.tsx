c95c58c90e8e5e66cf64e888154ef31817b0fc48
hendryriq <hendryriq@gmail.com>
2025-02-24 10:05:09 +0700

    update komen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import Layout from "@/components/layout/layout.module";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -77,7 +77,6 @@ export default function Product() {
           ))}
         </div>
       </Layout>
-      {/* <button onClick={logout}>Logout</button> */}
     </div>
   );
 }
