67fb040a2ba4c811a8f684911cfee32c45cd447d
hendryriq <hendryriq@gmail.com>
2025-02-24 10:08:44 +0700

    perbaikan kode

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { useRouter } from "next/router";
 
 export default function NotFoundPage() {
-  const router = useRouter();
-
   return(
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center"></div>
   );
